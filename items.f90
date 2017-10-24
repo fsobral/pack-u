f5d4818527868897d93cacd428a1e9b065e4d110
fsobral <fsobral@uem.br>
2017-10-24 17:09:33 +0100

    Added utility functions 'swapItems' and 'toVector'

--- items.f90
+++ items.f90
@@ -52,5 +52,72 @@ contains
     end do
     
   end subroutine createSameItems
+
+  ! ***********************************************************
+  ! ***********************************************************
+
+  subroutine swapItems(v, i, j)
+
+    ! Swaps positions 'i' and 'j' in the vector of items 'v'
+    
+    implicit none
+
+    ! SCALAR ARGUMENTS
+    integer :: i, j
+    
+    ! ARRAY ARGUMENTS
+    type(Item) :: v(:)
+
+    intent(in)    :: i, j
+    intent(inout) :: v
+    
+    ! LOCAL SCALARS
+    type(Item) :: tmp
+
+    tmp = v(i)
+
+    v(i) = v(j)
+
+    v(j) = tmp
+    
+  end subroutine swapItems
+
+  subroutine toVector(nItems, v, n, x)
+
+    ! This subroutine converts an array of items to a vector of x and
+    ! y positions.
+
+    implicit none
+    
+    ! SCALAR ARGUMENTS
+    integer :: nItems, n
+
+    ! ARRAY ARGUMENTS
+    type(Item) :: v(nItems)
+    real(8)    :: x(n)
+
+    intent(in)  :: nItems, v, n
+    intent(out) :: x
+
+    ! LOCAL SCALARS
+    integer :: i
+
+    if (n .ne. 2 * nItems) then
+
+       write(*, *) 'ERROR: mismatching dimensions in toVector(..)!'
+
+       return
+
+    end if
+
+    do i = 1, nItems
+
+       x(2 * i - 1) = v(i)%x
+
+       x(2 * i)     = v(i)%y
+
+    end do
+    
+  end subroutine toVector
   
 end module items
